f27c209a3a166b22927572865ae514aa4b44471c
yingying <chen.yingying.01988@gmail.com>
2018-07-24 17:27:05 +0100

    refactor bear name and type

--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -4,15 +4,15 @@ require_relative("../bear.rb")
 
 class BearTest < MiniTest::Test
   def setup
-    @bear_1 = Bear.new("Gina", "Panda" )
+    @bear_1 = Bear.new("Yogi", "Grizzly" )
   end
 
   def test_get_bear_name()
-    assert_equal("Gina", @bear_1.name())
+    assert_equal("Yogi", @bear_1.name())
   end
 
   def test_get_bear_type()
-    assert_equal("Panda", @bear_1.bear_type())
+    assert_equal("Grizzly", @bear_1.bear_type())
   end
 
 
